eb25334e468b93c75386d426c5f285d56bfc94d1
IT-gaurav <56316270+IT-gaurav@users.noreply.github.com>
2020-10-04 21:57:00 -0400

    login, register

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+
+  <nav>
+    <ul>
+      <a href="./view/login.php"><li>login</li></a>
+      <a href="./view/register.php"><li>register</li></a>
+    </ul>
+  </nav>    
+
+
+  </body>
+</html>
